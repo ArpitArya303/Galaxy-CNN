941b6e6b07398c1ea52b5289425054dab995c61e
Arpit Arya <arpit.arya@students.iiserpune.ac.in>
2025-04-27 04:08:30 +0530

    Modified predict file

--- interface.py
+++ interface.py
@@ -2,6 +2,6 @@
 
 from model import GalaxyCNN as TheModel
 from train import train_model as the_trainer
-from predict import classify_galaxies as the_predictor
+from predict import predict_galaxy as the_predictor
 from dataset import get_dataset as TheDataset
 from dataset import get_loader as the_dataloader
